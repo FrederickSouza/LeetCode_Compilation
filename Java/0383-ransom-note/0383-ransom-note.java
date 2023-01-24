32f1b12ebfcf0e803edaed266fc0dbc6bcaf3022
FrederickSouza <53228185+FrederickSouza@users.noreply.github.com>
2023-01-24 15:43:06 -0300

    Time: 2 ms (99.21%), Space: 42.9 MB (56.84%) - LeetHub

--- Java/0383-ransom-note/0383-ransom-note.java
+++ Java/0383-ransom-note/0383-ransom-note.java
@@ -1,32 +1,22 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> map = new HashMap();
+		if(ransomNote.length() > magazine.length()) {
+			return false;
+		}
 		
-		for(int i=0; i<ransomNote.length(); i++) {
-			char temp = ransomNote.charAt(i);
-			
-			if(map.get(temp) != null) {
-				map.put(temp, map.get(temp) + 1);
-			} else {
-				map.put(temp, 1);
+		int[] alphabet_counter = new int[26];
+		
+		for(Character c : magazine.toCharArray()) {
+			alphabet_counter[c - 'a']++;
+		}
+		
+		for(Character c : ransomNote.toCharArray()) {
+			if(alphabet_counter[c - 'a'] == 0 ) {
+				return false;
 			}
+			alphabet_counter[c - 'a']--;
 		}
 		
-		//printMap(map);
-		
-		for(int j=0; j<magazine.length(); j++) {
-			char letra = magazine.charAt(j);
-			
-			if(map.get(letra) != null && map.get(letra) > 0) {
-				map.put(letra, map.get(letra) -1);
-			}
-		}
-
-		Set<Integer> ans = new HashSet(map.values());
-		if(ans.size() == 1 && ans.contains(0)) {
-			return true;
-		}
-		
-		return false;
+		return true;
     }
 }
